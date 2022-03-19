9516eaa83f938a9a60fd3ebc6086eced6cf4219d
Aydın Terzi <aydinterzi7@gmail.com>
2022-03-19 13:19:00 +0300

    multiple layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { HomeComponent } from './ui/components/home/home.component';
+import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
+import { OrderModule } from './admin/components/order/order.module';
+import { ProductsModule } from './admin/components/products/products.module';
+import { LayoutComponent } from './admin/layout/layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'admin', component: LayoutComponent, children:[
+    {path:"",component:DashboardComponent},
+    {path: 'customers', loadChildren: () => import('./admin/components/customer/customer.module').then(m => m.CustomerModule)},
+    {path: 'products', loadChildren: () => import('./admin/components/products/products.module').then(m => m.ProductsModule)},
+    {path: 'orders', loadChildren: () => import('./admin/components/order/order.module').then(m => m.OrderModule)},
+  ]},
+  {path:"",component:HomeComponent},
+  {path:"basket",loadChildren:()=>import('./ui/components/basket/basket.module').then(m=>m.BasketModule)},
+  {path:"products", loadChildren:()=>import('./ui/components/products/products.module').then(m=>m.ProductsModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
